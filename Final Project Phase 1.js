1f7eec7deabad19de982988fd4d93bd4e61e08d2
qbarefoot <quintenbarefoot@gmail.com>
2021-06-03 21:46:15 -0400

    fixed spacing, functions all intact, working

--- Final Project Phase 1.js
+++ Final Project Phase 1.js
@@ -1,21 +1,14 @@
 
- function cal(val)
- {
-     document.getElementById("result").value+=val
- }
-   
- function solve()
- {
-     let x = document.getElementById("result").value
-     let y = eval(x)
-     document.getElementById("result").value = y
- }
+function cal(val) {
+    document.getElementById("result").value += val
+}
 
- function clr()
- {
-     document.getElementById("result").value = ""
- }
+function solve() {
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result").value = y
+}
 
-// when you click on a number button, calculates in to a result screen.
-
-// when you click on the equal button, it will return what results were inputed by the number/cal.
+function clr() {
+    document.getElementById("result").value = ""
+}
